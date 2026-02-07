3c15f289c82178db212b65c652ef2da7589daa03
Riteek Doble <pawarriteek78@gmail.com>
2026-02-07 18:25:28 +0530

    new inharitance program

--- inharitance.java
+++ inharitance.java
@@ -1,24 +1,53 @@
-public class inharitance {
-    int roll ,mark;
+ class Input {
+
+    int roll, mark;
     String name;
-    void input()
-    {
+
+    Input() {
         System.out.println("entr your roll number");
 
     }
-    
+
 }
-class ankit extends inharitance
+
+class ankit extends Input
 
 {
-    void output()
-    {
-        roll=1; name="riteek"; mark=89;
-        System.out.println(roll+" "+name+" "+mark);
-    }
-public static void name(String[]args) {
-    ankit r=new ankit();
-    r.input();
-r.output();}
-}
+    void output() {
+        roll = 1;
+        name = "ankit";
         
+        mark = 89;
+        System.out.println(roll + " " + name + " " + mark);
+    }
+}
+class riteek extends Input
+
+{
+    void output() {
+        roll = 2;
+        name = "riteek";
+        mark = 39;
+        System.out.println(roll + " " + name + " " + mark);
+    }
+}
+class rohit extends Input
+
+{
+    void output() {
+        roll = 3;
+        name = "rohit";
+        mark = 99;
+        System.out.println(roll + " " + name + " " + mark);
+    }
+}
+
+public class inharitance {
+    public static void main(String[] args) {
+        // rohit r = new rohit();
+        // // r.input();
+        // r.output();r.riteek();
+        Input obj=new Input();
+         
+    }
+}
