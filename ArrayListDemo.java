0f5b874f604174e91514b5c6f884c54671abd583
Riteek Doble <pawarriteek78@gmail.com>
2026-02-20 23:12:16 +0530

    new program added

---
+++ ArrayListDemo.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        // List<Integer> array=new ArrayList<>();
+        // array.add(12);
+        // System.out.println(array.get(0));
+        // array.add(1, 12);
+        // System.out.println(array);
+      
+        // System.out.println(array);
+        //      List<Integer>list=new ArrayList<>();
+        //      list.add(20);
+        //      list.add(50);
+        //      list.addAll(array);
+        //      System.out.println(list);
+        //      list.retainAll(list);
+        //      System.out.println(list);
+        //      list.addLast(20);
+        //      System.out.println(list);
+        // List<String> string=new ArrayList<>();
+        // string.add("riteek");
+        // string.set(0,"pappu");
+        // System.out.println(string.toArray());
+    //     Stack<String> string=new Stack<>();
+    //    string.addAll(Arrays.asList("riteek","rupesh","pappu","neha"));
+    //    System.out.println(string);
+//     LinkedList<Integer> linkedlist=new LinkedList();
+//     linkedlist.add(0);
+//     System.out.println(linkedlist.get(0)); 
+//     linkedlist.addFirst(10);
+//     System.out.println(linkedlist.add(20));
+//     System.out.println(linkedlist);
+//    int value= linkedlist.get(2);
+//    System.out.println(value);
+//   linkedlist.removeIf(x -> x == 0); 
+//   System.out.println(linkedlist);
+// Student<Integer> Student=new Studnet<>();
+Queue<Long> Qu=new LinkedList<Long>();
+Qu.offer(30L);
+
+    }
+}
