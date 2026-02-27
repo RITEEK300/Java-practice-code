67c62a2e0e8df0dbd1c2d83eb5f6d1220d6816ae
Riteek Doble <pawarriteek78@gmail.com>
2026-02-27 22:29:43 +0530

    new program

--- Reverse.java
+++ Reverse.java
@@ -1,9 +1,21 @@
-public class Reverse {
-    int a = 10;
-    int b = 20;
+import java.util.Scanner;
 
+public class Reverse {
+    // int a = 10;
+    // int b = 20;
+
+    // public static void main(String[] args) {
+    //     Reverse r = new Reverse();
+    //     System.out.println(r.a + " " + r.b);
+    // }
     public static void main(String[] args) {
-        Reverse r = new Reverse();
-        System.out.println(r.a + " " + r.b);
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+     int rev=0;   
+        while(n>0){
+rev=rev*10+n%10;
+n=n/10;
+        }
+System.out.println(rev);
     }
 }
