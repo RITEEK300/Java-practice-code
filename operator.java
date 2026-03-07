acd8f19b67cc95c55bff491357c3ff67aac7f851
Riteek Doble <pawarriteek78@gmail.com>
2026-03-07 23:03:19 +0530

    updated

--- operator.java
+++ operator.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
 public class operator {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int arr[]={1,3,4,5,6,5,45};
+    for(int e:arr){
+arr[e]=e^e;
+    }
+ 
+    }
 }
