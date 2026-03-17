c2aecdae509ace0a8f4690f324da1ce980dd7d39
Riteek Doble <pawarriteek78@gmail.com>
2026-03-17 13:52:02 +0530

    new program

--- FunctionalInterfaceDemo.java
+++ FunctionalInterfaceDemo.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+@FunctionalInterface
+interface Operation{
+    int sum(int a,int b);
+}
 
 public class FunctionalInterfaceDemo {
+    static void main() {
+        Operation obj=new Operation() {
+            @Override
+            public int sum(int a,int b) {
+                return a+b;
+            }
+        };
+        System.out.println(obj.sum(1,2));
+    }
 }
